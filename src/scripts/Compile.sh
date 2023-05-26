e308c9ddc07472b9abf4365075b1e28d015b6722
ryzhovalex <thed4rkof@gmail.com>
2023-05-26 11:42:45 +0300

    build: exit with error code 1 in Compile.sh

--- src/scripts/Compile.sh
+++ src/scripts/Compile.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if [ -z $1 ]; then
     echo "Usage: ./asm64 <asmMainFile> (no extension)"
-    exit
+    exit 1
 fi
 
 if [ ! -e "src/$1.asm" ]; then
     echo "Error, $1.asm not found."
     echo "Note, do not enter file extensions."
-    exit
+    exit 1
 fi
 
 yasm -Worphan-labels -g dwarf2 -f elf64 -o obj/debug/$1.o -l obj/debug/$1.lst src/$1.asm && ld -g -o bin/debug/$1 obj/debug/$1.o
